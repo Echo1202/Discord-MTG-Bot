c68335795860c341b3bb781bdeb5730e96dd5e0e
escho <escho437@gmail.com>
2019-06-02 15:30:12 -0700

    Initial Commit

--- red-mtg/red-mtg.py
+++ red-mtg/red-mtg.py
@@ -1,21 +1,45 @@
 import discord
 from .utils.dataIO_import_dataIO
-from__main__import_settings
+from __main__ import settings
+import requests
 
-class red-mtg:
+class RedMtg:
     def  __init__(self, bot):
         self.bot = bot
-        # load any local files here if desired
         self.owner = '<![]>'.format(settings.owner)
 
     async def listener(self, message):
-        # listener code here
-        channel = message.channel
-        author = message.author.id
+        channel = message.channel.id
 
-        if author == '127883009889140736' and channel == '224417319189872650':
-            if "hello" in message.content:
-                await self.bot.say('Hello There')
+        # restrict usage within a specific channel
+        if channel == '372159261108600833':
+            #check message for [[ ]]
+            msg_lower = message.content.lower()
+            if self.string_find(msg_lower, "[[") and self.string_find(msg_lower, "]]"):
+                #parse the message
+                name = msg_lower[msg_lower.find("[[")+2:msg_lower.find("]]")] #will find the name by +2("cutting out") the
+                                                                              #open brackets
+                url = 'https://api.scryfall.com/cards/named?fuzzy='
+                url += name
+                my_header = {'User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"}
+                # using the name get the json for the page (json is now a dict(?))
+                card_data = requests.get(url, headers=my_header, allow_redirects=True).json() #allow_redirects allow us
+                                                                                              #to go all the way to the content
+                try: # throws exceptions
+                    link = "https://scryfall.com/card/{}/{}".format(card_data["set"], card_data["collector_number"])
+                    embed_obj = discord.Embed(title=card_data["name"], url=link, description=card_data["oracle_text"])
+                    # embed_obj = discord.Embed(title="", url=card["scryfall_uri"])
+                    embed_obj.set_thumbnail(url=card_data["image_uris"]["png"])
+                    await self.bot.send_message(message.channel, embed=embed_obj)
+                    # await self.bot.send_message(message.channel, "Card: {} - Collector Number: {} - Set: {} - URL: {}".format(card["name"], card["collector_number"], card["set"], link))
+                except discord.Forbidden:
+                    return
+
+    def string_find(self, content, word):
+        i = content.find(word)
+        if i == -1:
+            return False
+        return True
 
 def setup(bot):
     n = red-mtg(bot)
