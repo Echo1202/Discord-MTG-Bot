f595697746ddadec39e3841a20cf09b7ac360f6a
escho <escho437@gmail.com>
2019-06-04 14:51:11 -0700

    Initial Commit

--- red-mtg/red-mtg.py
+++ red-mtg/red-mtg.py
@@ -13,27 +13,64 @@ class RedMtg:
 
         # restrict usage within a specific channel
         if channel == '372159261108600833':
-            #check message for [[ ]]
-            msg_lower = message.content.lower()
-            if self.string_find(msg_lower, "[[") and self.string_find(msg_lower, "]]"):
-                #parse the message
-                name = msg_lower[msg_lower.find("[[")+2:msg_lower.find("]]")] #will find the name by +2("cutting out") the
-                                                                              #open brackets
-                url = 'https://api.scryfall.com/cards/named?fuzzy='
-                url += name
-                my_header = {'User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"}
-                # using the name get the json for the page (json is now a dict(?))
-                card_data = requests.get(url, headers=my_header, allow_redirects=True).json() #allow_redirects allow us
-                                                                                              #to go all the way to the content
-                try: # throws exceptions
-                    link = "https://scryfall.com/card/{}/{}".format(card_data["set"], card_data["collector_number"])
-                    embed_obj = discord.Embed(title=card_data["name"], url=link, description=card_data["oracle_text"])
-                    # embed_obj = discord.Embed(title="", url=card["scryfall_uri"])
-                    embed_obj.set_thumbnail(url=card_data["image_uris"]["png"])
-                    await self.bot.send_message(message.channel, embed=embed_obj)
-                    # await self.bot.send_message(message.channel, "Card: {} - Collector Number: {} - Set: {} - URL: {}".format(card["name"], card["collector_number"], card["set"], link))
-                except discord.Forbidden:
-                    return
+            key,name = self.message_find(message)
+            url = 'https://api.scryfall.com/cards/named?fuzzy='
+            url += name
+
+            #try: # throws exceptions
+                #link = "https://scryfall.com/card/{}/{}".format(card_data["set"], card_data["collector_number"])
+                #embed_obj = discord.Embed(title=card_data["name"], url=link, description=card_data["oracle_text"] + card_data["mana_cost"])
+                #embed_obj.add_field(name = "type_line")
+                #embed_obj.set_thumbnail(url=card_data["image_uris"]["png"])
+                #await self.bot.send_message(message.channel, embed=embed_obj)
+            #except discord.Forbidden:
+                #return
+            self.key_display(key,name,channel)
+            except discord.Forbidden:
+                return
+
+    def message_find(self, message):
+        msg_lower = message.content.lower()
+        if self.string_find(msg_lower, "[[") and self.string_find(msg_lower, "]]"):
+            # parse the message
+            name = msg_lower[msg_lower.find("[[") + 2:msg_lower.find("]]")]
+            if self.string_find(name, "$"):
+                name = name[name.find("$") + 1]
+                return "$", name
+            if self.string_find(name, "!"):
+                name = name[name.find("!" + 1)]
+                return "!", name
+            if self.string_find(name, "?"):
+                name = name[name.find("?" + 1)]
+                return "?", name
+            else:
+                return "0", name
+
+    def key_display(self,key,name,channel):
+        my_header = {'User-Agent': "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"}
+        # using the name get the json for the page (json is now a dict(?))
+        card_data = requests.get(name, headers=my_header, allow_redirects=True).json()
+        if key == "$":
+            link = "https://scryfall.com/card/{}/{}".format(card_data["set"], card_data["collector_number"])
+            embed_obj = discord.Embed(title="Price of " + card_data["name"], url=link, description=card_data["set"] + " $" + card_data["prices"]["usd"])
+            await self.bot.send_message(channel.channel, embed=embed_obj)
+        if key == "!":
+            link = "https://scryfall.com/card/{}/{}".format(card_data["set"], card_data["collector_number"])
+            embed_obj = discord.Embed(title=card_data["name"], url=link)
+            embed_obj.set_thumbnail(url=card_data["image_uris"]["normal"])
+            await self.bot.send_message(channel.channel, embed=embed_obj)
+        if key == "?":
+            ruling_link = card_data["rulings_uri"]
+            embed_obj = discord.Embed(title="Ruling for " + card_data["name"], url=ruling_link)
+            for line in card_data:
+                embed_obj.add_field(name=card_data["published_at"], value=card_data["comment"], inline=False)
+            await self.bot.send_message(channel.channel, embed=embed_obj)
+        if key == "0":
+            link = "https://scryfall.com/card/{}/{}".format(card_data["set"], card_data["collector_number"])
+            embed_obj = discord.Embed(title=card_data["name"], url=link, description=card_data["oracle_text"] + card_data["mana_cost"])
+            embed_obj.add_field(name="type_line")
+            embed_obj.set_thumbnail(url=card_data["image_uris"]["png"])
+            await self.bot.send_message(channel.channel, embed=embed_obj
 
     def string_find(self, content, word):
         i = content.find(word)
